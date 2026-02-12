84f828a9c150caf07b34d2859e357233566b3990
Caike Henry <125682254+CaikeHenry007@users.noreply.github.com>
2026-02-12 20:34:19 -0300

    Create Vetor.java

---
+++ Vetor.java
@@ -0,0 +1,91 @@
+package listaestatica;
+
+import java.util.Arrays;
+
+public class Vetor {
+
+     private static final int CAPACIDADE_INICIAL = 5;
+     private Cliente[] clientes;
+     private int totalDeClientes;
+
+     public Vetor() {
+          clientes = new Cliente[CAPACIDADE_INICIAL];
+          totalDeClientes = 0;
+     }
+
+     private void garatirEspaco() {
+          if (totalDeClientes == clientes.length) {
+               clientes = Arrays.copyOf(clientes, clientes.length * 2);
+          }
+     }
+
+     public void adicionar(Cliente novoCliente) {
+          garatirEspaco();
+          clientes[totalDeClientes] = novoCliente;
+          totalDeClientes++;
+     }
+
+     public void adicionarPosicaoEspecifica(Cliente novoCliente, int posicaoEspecifica) {
+          garatirEspaco();
+          if (!posicaoValidaInserir(posicaoEspecifica)) {
+               throw new IllegalArgumentException("Posicao Invalida");
+          }
+          for (int i = totalDeClientes; i > posicaoEspecifica; i--) {
+               clientes[i] = clientes[i - 1];
+          }
+          clientes[posicaoEspecifica] = novoCliente;
+          totalDeClientes++;
+     }
+
+     public int pegarTotalClientes() {
+          return totalDeClientes;
+     }
+
+     public int tamanhoVetor() {
+          return clientes.length;
+     }
+
+     private boolean posicaoValida(int posicao) {
+          return posicao >= 0 && posicao < totalDeClientes;
+     }
+
+     private boolean posicaoValidaInserir(int posicao) {
+          return posicao >= 0 && posicao <= totalDeClientes;
+     }
+
+     public Cliente pegar(int posicao) {
+          if (!posicaoValida(posicao)) {
+               throw new IllegalArgumentException("Posicao Invalida");
+          }
+          return clientes[posicao];
+     }
+
+     public boolean contem(Cliente clienteBuscado) {
+          for (int i = 0; i < totalDeClientes; i++) {
+               if (clientes[i].equals(clienteBuscado)) {
+                    return true;
+               }
+          }
+          return false;
+     }
+
+     public void remover(int posicaoRemover) {
+          if (!posicaoValida(posicaoRemover)) {
+               throw new IllegalArgumentException("Posicao Invalida");
+          }
+          for (int i = posicaoRemover; i < totalDeClientes - 1; i++) {
+               clientes[i] = clientes[i + 1];
+          }
+          clientes[totalDeClientes - 1] = null;
+          totalDeClientes--;
+     }
+
+     public void removerUltimoElemento(int posicaoRemoverUltima) {
+          if (!posicaoValida(posicaoRemoverUltima)) {
+               throw new IllegalArgumentException("Posicao Invalida");
+          }
+          clientes[totalDeClientes - 1] = null;
+          totalDeClientes--;
+     }
+
+}
